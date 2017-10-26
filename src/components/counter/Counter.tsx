b24dddc13145a06b91b12019c7c599090da87af6
escsun <escsunarcher@gmail.com>
2017-10-26 12:24:04 +0300

    add dispatch actions for component counter

--- src/components/counter/Counter.tsx
+++ src/components/counter/Counter.tsx
@@ -2,12 +2,18 @@ import * as React from "react";
 
 interface Props {
   count: number;
+  onIncrement: () => void;
+  onDecrement: () => void;
 }
 
 const Counter = (props: Props) => {
   return (
     <div>
       Counter: {props.count}
+      <div className="btn-group">
+        <button onClick={props.onIncrement}>+</button>
+        <button onClick={props.onDecrement}>-</button>
+      </div>
     </div>
   );
 };
