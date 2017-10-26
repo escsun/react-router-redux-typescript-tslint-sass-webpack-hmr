c2c4dfd3182b94ab35365fcf7f3024c1ed22a541
escsun <escsunarcher@gmail.com>
2017-10-26 12:09:54 +0300

    add counter container to app

--- src/containers/app/App.tsx
+++ src/containers/app/App.tsx
@@ -1,14 +1,14 @@
 import * as React from "react";
 
-import Counter from "../../components/counter/Counter";
 import Hello from "../../components/hello/Hello";
+import CounterContainer from "../counter/Counter.";
 
 const App = (): JSX.Element => {
   return (
     <div>
       <h1>Welcome to React with Typescript</h1>
       <Hello name="React"/>
-      {/*<Counter />*/}
+      <CounterContainer />
     </div>
   );
 };
