0ae41a7bcab75a3b4ac5b88e702bb7f54d29a844
escsun <escsunarcher@gmail.com>
2017-10-26 13:22:24 +0300

    add main component for routes

---
+++ src/components/main/Main..tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import {
+  Route,
+  Switch
+} from "react-router-dom";
+import CounterContainer from "../../containers/counter/Counter.";
+import Hello from "../hello/Hello";
+
+const Main = (): JSX.Element => {
+  return (
+    <main>
+      <Switch>
+        <Route exact={true} path="/" render={(props) => <Hello {...props} name="React"/>} />
+        <Route path="/counter" component={CounterContainer}/>
+      </Switch>
+    </main>
+  );
+};
+
+export default Main;
