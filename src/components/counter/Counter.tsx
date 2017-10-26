c7d5b0b5b2d307c869025d5789b72aceafc5b252
escsun <escsunarcher@gmail.com>
2017-10-26 12:08:29 +0300

    update counter component to presentational

--- src/components/counter/Counter.tsx
+++ src/components/counter/Counter.tsx
@@ -1,37 +1,15 @@
 import * as React from "react";
 
-interface State {
+interface Props {
   count: number;
 }
 
-class Counter extends React.Component<{}, State> {
-  timerId: any;
-
-  constructor() {
-    super();
-
-    this.state = {
-      count: 0
-    };
-  }
-
-  componentWillMount() {
-    this.timerId = setInterval(() => {
-      this.setState({count: this.state.count + 1});
-    }, 1000);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.timerId);
-  }
-
-  render(): JSX.Element {
-    return (
-      <div>
-        Counter: {this.state.count}
-      </div>
-    );
-  }
-}
+const Counter = (props: Props) => {
+  return (
+    <div>
+      Counter: {props.count}
+    </div>
+  );
+};
 
 export default Counter;
