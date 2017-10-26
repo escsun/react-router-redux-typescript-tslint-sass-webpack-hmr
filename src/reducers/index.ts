08a75d2e13e03ca7c97c0a89a87844d1b801709f
escsun <escsunarcher@gmail.com>
2017-10-26 13:23:10 +0300

    add router reducer

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,12 +1,15 @@
+import * as fromRouter from "react-router-redux";
 import { combineReducers } from "redux";
 import * as fromCounter from "./counter";
 
 export interface RootState {
   counter: fromCounter.State;
+  routing: fromRouter.RouterState;
 }
 
 const rootReducer = combineReducers<RootState>({
-  counter: fromCounter.counterReducer
+  counter: fromCounter.counterReducer,
+  routing: fromRouter.routerReducer
 });
 
 export default rootReducer;
