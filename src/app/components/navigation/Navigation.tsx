900980f93947e2920b554309933cfb8b2b53c732
escsun <escsunarcher@gmail.com>
2017-12-09 20:29:15 +0200

    Replace Link to NavLink

--- src/app/components/navigation/Navigation.tsx
+++ src/app/components/navigation/Navigation.tsx
@@ -1,17 +1,18 @@
 import * as React from "react";
 
+import { NavLink } from "react-router-dom";
+
 import "./Navigation.scss";
-import { Link } from "react-router-dom";
 
 const Navigation = (): JSX.Element => {
   return (
     <nav className="Navigation">
       <ul className="Navigation__items">
         <li className="Navigation__item">
-          <Link to="/" className="Navigation__link link">Home</Link>
+          <NavLink to="/" exact={true} className="Navigation__link link">Home</NavLink>
         </li>
         <li className="Navigation__item">
-          <Link to="/about" className="Navigation__link link">About</Link>
+          <NavLink to="/about" className="Navigation__link link">About</NavLink>
         </li>
       </ul>
     </nav>
